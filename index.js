9015dd32540f365084b50e9e5c82984e2894c5d5
Milk-Cool <milan-pl@mail.ru>
2023-08-31 16:49:58 +0300

    just testing

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const { join } = require("path");
 
 const app = express();
 
-app.use("/schedule", express.static(join(__dirname, "html")));
-app.get("/schedule", (req, res) => {
+app.use("/megaaboba", express.static(join(__dirname, "html")));
+app.get("/megaaboba", (req, res) => {
     res.sendFile(join(__dirname, "html/index.html"));
 });
 
